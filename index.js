2e6130c23456486c855df19bb93d5b2aa3e34634
turtleking231 <156595303+turtleking231@users.noreply.github.com>
2024-06-27 20:23:40 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+document.getElementById("but").onclick = function() {
+  let num = Math.floor(Math.random() * 6)
+  console.log(num)
+  if (parseInt(document.getElementById("ans").value) === num) {
+    document.getElementById("c/w").innerHTML = "correct! click on the button to guess again"
+  }
+  else {
+    document.getElementById("c/w").innerHTML = "wrong, click on the button to guess again, it will be a diffirent number tho!"
+  }
+}
+
+
+
