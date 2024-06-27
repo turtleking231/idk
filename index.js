be59419f3e1a695e0fc38406bd96044e6a8870ac
turtleking231 <156595303+turtleking231@users.noreply.github.com>
2024-06-27 20:57:42 +0100

    index.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 
 
-document.getElementById("but").onclick = function() {
-  let num = Math.floor(Math.random() * 6);
-console.log(num);
-  if (parseInt(document.getElementById("ans").value) === num) {
-    document.getElementById("c/w").innerHTML = "correct! click on the button to guess again";
-  } else {
-    document.getElementById("c/w").innerHTML = "wrong, click on the button to guess again, it will be a different number tho!";
+let num = Math.floor(Math.random() * 6);
+  console.log(num);
+
+  document.getElementById("but").onclick = function() {
+    if (parseInt(document.getElementById("ans").value) === num) {
+      document.getElementById("c/w").innerHTML = "correct! click on the button to guess again";
+    } else {
+      document.getElementById("c/w").innerHTML = "wrong, click on the button to guess again, it will be a different number tho!";
+    }
+    num = Math.floor(Math.random() * 6); // Generate a new number after each guess
+    console.log(num); // Log the new number immediately
   }
-};
