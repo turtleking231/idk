6b5c8bda7d0cdc3b94f98e8b1e7110056cdb8bf9
turtleking231 <156595303+turtleking231@users.noreply.github.com>
2024-06-28 11:55:30 +0100

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 
-
-
-
-let num = Math.floor(Math.random() * 6);
-  console.log(num);
-
-  document.getElementById("but").onclick = function() {
-    if (parseInt(document.getElementById("ans").value) === num) {
-      document.getElementById("c/w").innerHTML = "correct! click on the button to guess again";
-    } else {
-      document.getElementById("c/w").innerHTML = "wrong, click on the button to guess again, it will be a different number tho!";
-    }
-    num = Math.floor(Math.random() * 6); // Generate a new number after each guess ( yes i used chat gpt and instead of removing it, added this)
-    console.log(num); // Log the new number immediately
+document.getElementById("but").onclick = function() {
+  let num = Math.floor(Math.random() * 6)
+  console.log(num)
+  if (parseInt(document.getElementById("ans").value) === num) {
+    document.getElementById("c/w").innerHTML = "correct! click on the button to guess again"
   }
+  else {
+    document.getElementById("c/w").innerHTML = "wrong, click on the button to guess again, it will be a diffirent number tho!"
+  }
+}
+
